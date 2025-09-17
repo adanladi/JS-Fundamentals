273c31ab8e82c5ac67b2fb52e618c276fa7f53f6
Amos Danladi <A0075797@na.hhcpr.hilton.com>
2025-09-17 01:23:01 +0100

    solution to task 4

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+// Create an array of strings
+const messages = [
+    "C is fun",
+    "Python is cool",
+    "JavaScript is amazing"
+];
+
+// Use join() to combine all messages with newlines
+console.log(messages.join('\n'));
