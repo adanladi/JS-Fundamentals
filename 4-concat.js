4499882378453c565e4dbda7bb87947e5047abd3
Amos Danladi <A0075797@na.hhcpr.hilton.com>
2025-09-17 01:15:14 +0100

    solution to task 4

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+// Get the first and second arguments
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print the formatted string
+console.log(`${firstArg} is ${secondArg}`);
