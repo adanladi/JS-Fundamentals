3087e09344c7a99fc23039995eaafc8a1d689365
Amos Danladi <A0075797@na.hhcpr.hilton.com>
2025-09-17 01:13:19 +0100

    solution to task 3

---
+++ 3-value_argument.js
@@ -0,0 +1,6 @@
+// Check if there are any arguments after node and script path
+if (process.argv[2]) {
+    console.log(process.argv[2]);  // Print the first argument
+} else {
+    console.log("No argument");    // No arguments found
+}
